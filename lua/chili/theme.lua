e06fa4ccc397c8d93f1aa1e7066d1c372f696864
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-10-18 05:21:03 -0500

    @lsp.type.parameter.dockerfile setted 2nd

--- lua/chili/theme.lua
+++ lua/chili/theme.lua
@@ -290,7 +290,7 @@ local function get_highlights(data, custom)
     ['@lsp.typemod.variable.defaultLibrary'] = { link = '@variable.builtin' },
     ['@lsp.typemod.variable.global'] = { fg = t.constant.fg, bold = true },
     ['@lsp.mod.deprecated'] = { strikethrough = true },
-    ['@lsp.type.parameter.dockerfile'] = { fg = t.identifier },
+    ['@lsp.type.parameter.dockerfile'] = { fg = p.syntax.red },
 
     -- LSP
     ['DiagnosticOk'] = { fg = custom.ok },
